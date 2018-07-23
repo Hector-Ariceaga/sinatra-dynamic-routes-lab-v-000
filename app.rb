2910a00d9b3b02c2e5c5973e11ba6a2c19af4936
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-07-23 16:09:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -34,7 +34,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-
+    binding.pry
     if @operation == "add"
       @number1.send ('+', @number2)
     elsif @operation == "subtract"
