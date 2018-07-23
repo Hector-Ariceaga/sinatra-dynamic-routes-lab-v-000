e1fc63d9f0bb71f022915986e91333a43238b227
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-07-23 15:50:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,8 +16,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    binding.pry
-    @number.tap {|n| "#{@phrase}"}
+    @number.times {puts "#{@phrase}"}
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
